deece314f0ecb087251ff59b6fc822277693e80b
marco-m97 <72286186+marco-m97@users.noreply.github.com>
2022-11-20 22:03:09 -0600

    Revision 1

---
+++ src/app/models/administrador.ts
@@ -0,0 +1,11 @@
+export class Administrador{
+
+    constructor(
+        public cedula:string,
+        public nombre:string,
+        public apellido1:string,
+        public apellido2:string,
+        public rol:string,
+        public contrasena:string
+    ){}
+}
